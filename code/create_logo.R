45d80d0e27fbf9086b2b8f8d55cb69431957d23b
jannes-m <malnamalja@gmx.de>
2018-07-21 14:03:01 +0200

    hexagon stuff

--- code/create_logo.R
+++ code/create_logo.R
@@ -23,7 +23,7 @@ library(grid)
 library(gridBase)
 library(gridExtra)
 # attach data
-rlogo = brick("pres/img/Rlogo.tif")
+rlogo = brick("pres/rmd/img/Rlogo.tif")
 
 # create hexagon
 # https://github.com/mkearney/hexagon
@@ -46,13 +46,24 @@ gr = st_graticule(ndiscr = 1000) %>%
 # browseURL("https://stat.ethz.ch/pipermail/r-help//2009-March/419787.html")
 
 graphics.off()
-png("pres/img/geocomp_logo.png", res = 300, width = 6, height = 6,
+
+png("pres/rmd/img/geocomp_logo_hex.png", res = 300, width = 6, height = 6,
     units = "cm")
 plot.new()
 pushViewport(viewport(x = 0, y = 0, width = 1, height = 1,
                       just = c(0, 0), name = 'base'))
-pushViewport(viewport(x = 0, y = 0.3,
-                      width = 0.7, height = 0.7,
+par(mar = rep(1.5, 4), xaxs = 'i', yaxs = 'i')
+par(mar = rep(0.3, 4), xaxs = "i", yaxs = "i")
+par(new = TRUE, fig = gridFIG())
+plot(hex_1, type = "l", col = "#001030", lwd = 1.5, axes = FALSE)
+# ggplot(hex_1, aes(x, y)) +
+#   geom_polygon(fill = NA, colour = "#001030", size = 1) +
+#   coord_fixed(ratio = 1, expand = TRUE) +
+#   coord_cartesian(xlim = range(hex_1$x), ylim = range(hex_1$y)) +
+#   theme_void()
+
+pushViewport(viewport(x = 0.17, y = 0.46,
+                      width = 0.4, height = 0.4,
                       just = c(0, 0)))
 # CHANGE
 par(mar = rep(0, 4), xaxs = 'i', yaxs = 'i')
@@ -67,8 +78,15 @@ plot(st_geometry(world_laea), bg = "white", col = "lightgray", add = TRUE)
 seekViewport("base")
 
 # create lower viewport
-pushViewport(viewport(x = 0.48, y = 0, width = 0.5, height = 0.5, 
+pushViewport(viewport(x = 0.43, y = 0.2, width = 0.39, height = 0.39, 
                       just = c(0, 0)))
 par(new = TRUE, fig = gridFIG())
 plotRGB(rlogo)
+
+# get toplevel view
+seekViewport("base")
+par(mar = rep(0.3, 4), xaxs = 'i', yaxs = 'i')
+par(new = TRUE, fig = gridFIG())
+plot(hex_1, type = "l", col = "#001030", lwd = 4, axes = FALSE)
+
 dev.off()
